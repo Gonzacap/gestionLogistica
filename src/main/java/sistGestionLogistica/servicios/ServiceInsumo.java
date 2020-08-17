1f9d12b66849482938c0e66e904834fea73aca67
milton1808 <milton_mansilla_18@hotmail.com>
2020-08-17 17:12:27 -0300

    buscar insumo

---
+++ src/main/java/sistGestionLogistica/servicios/ServiceInsumo.java
@@ -0,0 +1,60 @@
+package sistGestionLogistica.servicios;
+
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import sistGestionLogistica.dao.InsumoDao;
+import sistGestionLogistica.dao.InsumoDaoMysql;
+import sistGestionLogistica.dominio.Camion;
+import sistGestionLogistica.dominio.Insumo;
+
+public class ServiceInsumo {
+	private InsumoDao insumoDao = new InsumoDaoMysql();
+
+	public ServiceInsumo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Insumo crearInsumo(Insumo i) throws SQLException {
+				return this.insumoDao.save(i);
+		}
+	
+	public Insumo editarInsumo(Insumo i) throws SQLException {
+		return this.insumoDao.update(i);
+	}
+	
+	public void borrar(Integer i) throws SQLException {
+		this.insumoDao.borrar(i);
+	}
+	public List<Insumo> buscarInsumo(Insumo i) throws SQLException {
+			
+			//inicializamos predicados
+			Predicate<Insumo> filtroId = (t) -> (true);
+			Predicate<Insumo> filtroDescripcion = (t) -> (true);
+			Predicate<Insumo> filtroUnidadMedida = (t) -> (true);
+			Predicate<Insumo> filtroCosto = (t) -> (true);
+			Predicate<Insumo> filtroPrecio = (t) -> (true);
+			Predicate<Insumo> filtroTipo = (t) -> (true);
+			
+			//Aplicamos Filtros solo si corresponde
+			if(i.getIdInsumo()>=0) filtroId = (t) -> (i.getIdInsumo().equals(t.getIdInsumo()));
+			if(!i.getDescripcion().equals("")) filtroDescripcion = (t) -> (i.getDescripcion().equals(t.getDescripcion()));
+			if(i.getCosto()>=0) filtroCosto = (t) -> (i.getCosto().equals(t.getCosto()));
+			if(i.getPrecio()>=0) filtroPrecio = (t) -> (i.getPrecio().equals(t.getPrecio()));
+		
+			//falta unidad de medida
+			
+			
+			
+			
+			
+			return insumoDao.buscarTodos().stream().filter(filtroId)
+					.filter(filtroDescripcion).filter(filtroCosto)
+					.filter(filtroPrecio).collect(Collectors.toList());
+		}
+
+}
