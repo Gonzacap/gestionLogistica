18dd94f57aad051d584f2b7aaa8b3b9bf4a9803b
milton1808 <milton_mansilla_18@hotmail.com>
2020-08-19 22:19:24 -0300

    metodo faltantes() en StockInsumoController

--- src/main/java/sistGestionLogistica/controller/StockInsumoController.java
+++ src/main/java/sistGestionLogistica/controller/StockInsumoController.java
@@ -1,9 +1,12 @@
 package sistGestionLogistica.controller;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import excepciones.DatosInvalidosException;
 import sistGestionLogistica.dominio.Insumo;
+import sistGestionLogistica.dominio.InsumoGeneral;
+import sistGestionLogistica.dominio.InsumoLiquido;
 import sistGestionLogistica.dominio.Planta;
 import sistGestionLogistica.dominio.StockInsumo;
 import sistGestionLogistica.servicios.ServiceInsumo;
@@ -53,6 +56,36 @@ public class StockInsumoController {
 		ServiceStockInsumo ssi=new ServiceStockInsumo();
 		
 		return ssi.existeStock(idPlanta, idInsumo);
+	}
+	
+	
+	//devuelve una matriz con columnas ID_PLANTA, ID_INSUMO, NOMBRE_PLANTA, DESCRIPCION_INSUMO, CANTIDAD, PTO_PEDIDO, STOCK_TOTAL	
+	public String[][] faltante(String idPlanta, String idInsumo){	// para buscar todos enviar faltantes("","")
+		Integer idPlan=-1, idIns= -1;
+		ServiceStockInsumo ssi=new ServiceStockInsumo();
+		//Formateamos los parametros de busqueda
+		if(!idPlanta.isBlank()) idPlan = Integer.valueOf(idPlanta);
+		if(!idInsumo.isBlank()) idIns = Integer.valueOf(idInsumo);
+		//buscamos las plantas que tengan faltantes
+		return this.aMatriz(ssi.faltantes(idPlan, idIns));
+	}
+	
+	public String[][] aMatriz(List<StockInsumo> listaStockInsumo){
+		String[][] matriz = new String[listaStockInsumo.size()][7];
+		ServiceStockInsumo ssi=new ServiceStockInsumo();		
+		
+		for(int i=0; i<listaStockInsumo.size();i++) {
+			matriz[i][0]= listaStockInsumo.get(i).getPlanta().getId().toString();
+			matriz[i][1]= listaStockInsumo.get(i).getInsumo().getIdInsumo().toString();
+			matriz[i][2]= listaStockInsumo.get(i).getPlanta().getNombre();
+			matriz[i][3]= listaStockInsumo.get(i).getInsumo().getDescripcion();
+			matriz[i][4]= listaStockInsumo.get(i).getCantidad().toString();
+			matriz[i][5]= listaStockInsumo.get(i).getPuntoReposicion().toString();
+			matriz[i][6]= ssi.stockTotal(listaStockInsumo.get(i).getInsumo().getIdInsumo()).toString();
+
+		}
+		
+		return matriz;
 	}
 
 }
--- src/main/java/tests/StockInsumoControllerTest.java
+++ src/main/java/tests/StockInsumoControllerTest.java
@@ -15,10 +15,13 @@ public class StockInsumoControllerTest {
 	@Test
 	public void test() throws DatosInvalidosException, SQLException {
 		StockInsumoController sic= new StockInsumoController();
-		sic.agregarStockInsumo("3", "8", "23", "10");
+//		sic.agregarStockInsumo("3", "3", "23", "100");
 		ServiceStockInsumo ssi= new ServiceStockInsumo();
 		System.out.println(ssi.stockTotal(8));
 		
+		String[][] m= sic.faltante("", "");
+		
+		System.out.println(m);
 	}
 
 }
