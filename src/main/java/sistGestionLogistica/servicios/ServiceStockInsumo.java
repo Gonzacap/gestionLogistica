c4ce9a717c5e4c7ce7b436cf264a74b67a3e5375
milton1808 <milton_mansilla_18@hotmail.com>
2020-08-25 19:40:47 -0300

    plantas con Stock en Service Stock

--- src/main/java/sistGestionLogistica/servicios/ServiceStockInsumo.java
+++ src/main/java/sistGestionLogistica/servicios/ServiceStockInsumo.java
@@ -1,6 +1,7 @@
 package sistGestionLogistica.servicios;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -8,6 +9,7 @@ import java.util.stream.Collectors;
 import sistGestionLogistica.dao.StockInsumoDao;
 import sistGestionLogistica.dao.StockInsumoDaoMysql;
 import sistGestionLogistica.dominio.Camion;
+import sistGestionLogistica.dominio.Planta;
 import sistGestionLogistica.dominio.StockInsumo;
 
 
@@ -41,6 +43,23 @@ public class ServiceStockInsumo {
 		
 	
 		return stockDao.faltantes().stream().filter(filtroIdPlanta).filter(filtroIdInsumo).collect(Collectors.toList());
+	}
+	
+	//Argumento: lista de Insumo-Cantidad [][2]
+	public List<Planta> plantasConStock(Integer[][] listaInsumosCantidad){
+		List<Planta> plantasFinal=new ArrayList<Planta>();
+		if(listaInsumosCantidad[0][0] != null) {
+			plantasFinal = stockDao.plantasConInsumo(listaInsumosCantidad[0][0], listaInsumosCantidad[0][1]);
+			
+			for(int i = 1; i< listaInsumosCantidad.length; i++ ) {
+				List<Planta> aux =stockDao.plantasConInsumo(listaInsumosCantidad[i][0], listaInsumosCantidad[i][1]);
+				plantasFinal = new ArrayList<Planta>(aux.stream()
+								.filter(plantasFinal::contains).collect(Collectors.toList()));
+			}
+		}
+		
+		
+		return plantasFinal;
 	}
 
 }
