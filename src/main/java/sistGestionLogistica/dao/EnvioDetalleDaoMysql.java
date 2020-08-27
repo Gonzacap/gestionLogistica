e949d6fe1d2c51f11d0c0fdef79e9ccf5de3306a
milton1808 <milton_mansilla_18@hotmail.com>
2020-08-27 18:54:14 -0300

    lista de rutas en daoEnvioPedido

--- src/main/java/sistGestionLogistica/dao/EnvioDetalleDaoMysql.java
+++ src/main/java/sistGestionLogistica/dao/EnvioDetalleDaoMysql.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import sistGestionLogistica.connection.DB;
 import sistGestionLogistica.dominio.EnvioDetalle;
+import sistGestionLogistica.dominio.Ruta;
 import sistGestionLogistica.servicios.ServiceCamion;
 
 
@@ -29,6 +30,14 @@ public class EnvioDetalleDaoMysql implements EnvioDetalleDao {
 			ps.setInt(1, envio.getNumOrden() );
 			ps.setInt(2, envio.getCamionAsignado().getId() );
 			ps.setDouble(3, envio.getCostoEnvio());
+			
+			if(envio.getRutaAsignada() !=  null) {
+				for(int i=0; i< envio.getRutaAsignada().size(); i++) {
+					//guardamos la ruta asignada ordenada
+					this.agregarRutaPedido(envio.getRutaAsignada().get(i).getIdRuta(),envio.getNumOrden() , i);
+				}
+			}
+			
 		
 			ps.executeUpdate();
 		} catch (SQLException e) {
@@ -99,6 +108,7 @@ public class EnvioDetalleDaoMysql implements EnvioDetalleDao {
 				envio.setNumOrden(rs.getInt("numOrden"));
 				envio.setCamionAsignado(sc.buscarPorId(rs.getInt("camionAsignado")));;
 				envio.setCostoEnvio(rs.getDouble("costoEnvio"));
+				envio.setRutaAsignada(this.obtenerListaRutaPedido(envio.getNumOrden()));
 				lista.add(envio);
 			}
 			
@@ -134,6 +144,7 @@ public class EnvioDetalleDaoMysql implements EnvioDetalleDao {
 				envio.setNumOrden(rs.getInt("numOrden"));
 				envio.setCamionAsignado(sc.buscarPorId(rs.getInt("camionAsignado")));;
 				envio.setCostoEnvio(rs.getDouble("costoEnvio"));
+				envio.setRutaAsignada(this.obtenerListaRutaPedido(envio.getNumOrden()));
 				lista.add(envio);
 			}
 			
@@ -144,6 +155,68 @@ public class EnvioDetalleDaoMysql implements EnvioDetalleDao {
 			try {
 				if(ps!=null) ps.close();
 				if(con!=null) con.close();				
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return lista;
+	}
+	private void agregarRutaPedido(Integer idRuta, Integer numOrden, Integer orden) {
+		Connection con2=null;
+		PreparedStatement ps2 = null;
+		String insert = "INSERT INTO pertenecea(idRuta,numOrden,orden) VALUES(?,?,?) ";
+		try {
+			con2 = DB.getConexion();
+			System.out.println("EJECUTA INSERT");
+			ps2 = con2.prepareStatement(insert);
+
+			ps2.setInt(1, idRuta);
+			ps2.setInt(2, numOrden );
+			ps2.setDouble(3, orden);
+		
+			ps2.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				if (ps2 != null)
+					ps2.close();
+				if (con2 != null)
+					con2.close();
+
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	public List<Ruta> obtenerListaRutaPedido(Integer numOrden){
+		Connection con2=null;
+		PreparedStatement ps2 = null;
+		ResultSet rs2 = null;
+		List<Ruta> lista= new ArrayList<Ruta>();
+		String buscar = "SELECT * FROM pertenecea WHERE numOrden = ? ORDER BY orden ASC";
+		RutaDao daoRuta = new RutaDaoMysql();
+		try {
+			con2 = DB.getConexion();
+			ps2 = con2.prepareStatement(buscar);
+			ps2.setInt(1,numOrden);
+			rs2 = ps2.executeQuery();
+			while(rs2.next()){
+				lista.add(daoRuta.buscarPorId(rs.getInt("idRuta")));
+			}
+			
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		finally {
+			try {
+				if(ps2!=null) ps2.close();
+				if(con2!=null) con2.close();				
 			}catch(SQLException e) {
 				e.printStackTrace();
 			}
