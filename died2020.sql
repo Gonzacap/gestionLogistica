451389630f161ee5506c077e9273636d629b51dc
milton1808 <milton_mansilla_18@hotmail.com>
2020-08-27 19:27:51 -0300

    died2020.sql

--- died2020.sql
+++ died2020.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-08-2020 a las 00:23:12
+-- Tiempo de generación: 28-08-2020 a las 00:26:54
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.33
 
