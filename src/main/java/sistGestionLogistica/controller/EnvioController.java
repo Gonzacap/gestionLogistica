c01d35e220b149f23dc1105dc58a68d573457365
milton1808 <milton_mansilla_18@hotmail.com>
2020-08-27 21:04:09 -0300

    envio controller

--- src/main/java/sistGestionLogistica/controller/EnvioController.java
+++ src/main/java/sistGestionLogistica/controller/EnvioController.java
@@ -1,5 +1,38 @@
 package sistGestionLogistica.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import sistGestionLogistica.dominio.EnvioDetalle;
+import sistGestionLogistica.dominio.ItemDetalle;
+import sistGestionLogistica.servicios.ServiceEnvioDetalle;
+
 public class EnvioController {
+	
+	public String[][] buscarItems(String numOrden ) throws SQLException{
+		Integer numeroOrden = Integer.valueOf(numOrden);
+		ServiceEnvioDetalle sed = new ServiceEnvioDetalle ();
+		
+		return this.aMatriz(sed.buscarPorNumOrden(numeroOrden));
+	}
+	
+	
+	
+	//Muestro info del envio 
+	public String[][] aMatriz(EnvioDetalle envio){
+		
+		String[][] matriz = new String[1][4];
+		
+	
+			
+			matriz[1][0]= envio.getNumOrden().toString();
+			matriz[1][1]= envio.getCamionAsignado().getPatente().toString();
+			matriz[1][2]= envio.getCostoEnvio().toString();
+			
+		
+		
+		return matriz;
+	}
+	
 
 }
